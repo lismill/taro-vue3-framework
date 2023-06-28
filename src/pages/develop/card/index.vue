df35d0b32920a68173b6ea861193ac6377b24314
lismill <lismill@163.com>
2023-06-28 09:20:05 +0800

    feat: icon 图标

--- src/pages/develop/card/index.vue
+++ src/pages/develop/card/index.vue
@@ -2,14 +2,15 @@
   <l-layout>
     <l-card v-bind="config">
       这里是内容
-      <template #more><Follow width="16px" color="#e74e3d"></Follow></template>
+      <template #more>
+        <IconFont name="follow" size="16" color="#e74e3d"></IconFont>
+      </template>
     </l-card>
   </l-layout>
 </template>
 
 <script setup lang="ts">
 import { ILCard } from "@/components/l-card/interface";
-import { Follow } from "@nutui/icons-vue";
 
 const config = reactive<ILCard>({ title: "123" });
 </script>
